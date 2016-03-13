71e85393234effd2953768566ea75c8be128fa43
zhouf fang <307994635@qq.com>
2016-03-14 00:53:27 +0800

    Add pmtest8

--- chapter3/pmtest8/pmtest8.asm
+++ chapter3/pmtest8/pmtest8.asm
@@ -1,17 +1,29 @@
 ; ==========================================
-; pmtest7.asm
-; 分页机制进阶，根据内存数量设计分页
+; pmtest8.asm
+; 分页机制终章，切换分页
 ; ==========================================
 
 %include "pm.inc"  ; 常量, 宏, 以及一些说明
 
-PageDirBase    equ    200000h ; PDE的开始地址：0x200000h(2M)
-PageTblBase    equ    201000h ; PTE的开始地址：0x201000h(2M + 4k)
+PageDirBase0    equ    200000h ; PDE的开始地址：0x200000h(2M)
+PageTblBase0    equ    201000h ; PTE的开始地址：0x201000h(2M + 4k)
+PageDirBase1    equ    210000h ; PDE的开始地址：0x200000h(2M + 64k)
+PageTblBase1    equ    211000h ; PTE的开始地址：0x201000h(2M + 64k + 4k)
 
-PageTestBase    equ    000h ; 测试修改页的起始地址
+PageTestBase    equ    0FFFh ; 测试修改页的起始地址
+
+LinearDemoAddr      equ    00401000h
+ProcFooAddr         equ    00401000h
+ProcBarAddr         equ    00501000h
+ProcPagingDemoAddr  equ    00301000h
 
 org    0100h
        jmp    LABEL_BEGIN     ; 接下来的是gdt数据部分，不是代码，必须要跳过去
+       ; "jmp LABEL_BEGIN"不改变CS，所以在没有长跳转之前CS一直是0100H。后面令DS=CS;
+       ; 后面的"add eax, LABEL_GDT"中"LABEL_GDT"是相对于DS（0x0100H）的偏移，得到绝对地址。
+       ; "add eax,LABEL_SEG_CODE32"中也一样，所以可以确定，nasm中所有的LABEL的都是相对于当前DS的偏移。
+
+       ; label 本身是按照距离程序首部开始计算的。而且16位的汇编程序编译成bin文件之后，汇编程序的组织结构和最终内存结构几乎一样（16位汇编程序的段首地址是16的整数倍，段末尾不够长度的补0）
 
 
 ; ===================   GDT   =======================
@@ -19,13 +31,13 @@ org    0100h
 [SECTION .gdt]
 ; GDT
 ;                                     段基址                段界限    属性
-LABEL_GDT:            Descriptor          0,                   0,    0        ; 空描述符
-LABEL_DESC_NORMAL:	  Descriptor	      0,              0ffffh, DA_DRW	  ; Normal描述符
-LABEL_DESC_PAGE_DIR:  Descriptor    PageDirBase,            4095, DA_DRW      ; Page Directory Entry，4k，段界限=4096-1， 段基址=0x0200000h
-LABEL_DESC_PAGE_TBL:  Descriptor    PageTblBase,     4096 * 8 -1, DA_DRW      ; Page Table Entry，这次只映射8个页表一个页表是4M，因此一共是32M，段基址=0x0201000h,段界限=8*4k-1
-LABEL_DESC_CODE32:    Descriptor          0,    SegCode32Len - 1, DA_C + DA_32 ; 非一致代码段, 32位代码段
-LABEL_DESC_CODE16:    Descriptor          0,              0ffffh, DA_C         ; 非一致代码段, 16位代码段
-LABEL_DESC_DATA:	  Descriptor	      0,	     DataLen - 1, DA_DRW       ; Data
+LABEL_GDT:            Descriptor          0,                   0,    0         ; 空描述符
+LABEL_DESC_NORMAL:	  Descriptor	      0,              0ffffh, DA_DRW	   ; Normal描述符
+LABEL_DESC_FLAT_C:    Descriptor          0,             0fffffh, DA_CR | DA_32 | DA_LIMIT_4K ; 4G
+LABEL_DESC_FLAT_RW:   Descriptor          0,             0fffffh, DA_DRW | DA_LIMIT_4K ; 4G
+LABEL_DESC_CODE32:    Descriptor          0,    SegCode32Len - 1, DA_CR + DA_32 ; 非一致代码段, 32位代码段
+LABEL_DESC_CODE16:    Descriptor          0,              0ffffh, DA_C          ; 非一致代码段, 16位代码段
+LABEL_DESC_DATA:	  Descriptor	      0,	     DataLen - 1, DA_DRW        ; Data
 LABEL_DESC_STACK:	  Descriptor	      0,          TopOfStack, DA_DRWA + DA_32	; Stack, 32 位
 LABEL_DESC_VIDEO:     Descriptor    0B8000h,              0ffffh, DA_DRW       ; 数据段，显存首地址
 
@@ -37,8 +49,8 @@ GdtPtr        dw     GdtLen - 1       ; GDT界限=GdtLen-1？段界限=段内的
 
 ; GDT 选择子
 SelectorNormal		equ	LABEL_DESC_NORMAL	   - LABEL_GDT
-SelectorPageDir     equ LABEL_DESC_PAGE_DIR    - LABEL_GDT
-SelectorPageTbl     equ LABEL_DESC_PAGE_TBL    - LABEL_GDT
+SelectorFlatC       equ LABEL_DESC_FLAT_C      - LABEL_GDT
+SelectorFlatRW      equ LABEL_DESC_FLAT_RW     - LABEL_GDT
 SelectorCode32		equ	LABEL_DESC_CODE32	   - LABEL_GDT
 SelectorCode16		equ	LABEL_DESC_CODE16	   - LABEL_GDT
 SelectorData		equ	LABEL_DESC_DATA		   - LABEL_GDT
@@ -77,10 +89,17 @@ _ARDStruct:         ; Address Range Descriptor Structure
     _dwLengthHigh:     dd    0
     _dwType:           dd    0
 
+_PageTableNumber       dd    0
+
 _MemChkBuf:    times   256  db  0
 
 
 ; ---------------- 保护模式下使用以下符号:
+; 因为程序是在实模式下编译的，上面的地址都是实模式下的地址，在保护模式下面，数据的地址是相对于段基址的offset。
+; 比如：上面的"_szPMMessage"相当于那一行的"$"，"$"指向当前行相对于段基址(设为var)的偏移地址。即"$=var+msg_offset"
+; "$$"指向当前section相对于段基址的偏移地址，即"$$=var+sec_offset"
+; 所以，"szPMMessage = _szPMMessage-$$" = "szPMMessage = (var+msg_offset) - (var+sec_offset)"
+; 最后，"szPMMessage = msg_offset - sec_offset"，最后寻址的时候加上正确的段基址即可。
 
 szPMMessage     equ     _szPMMessage - $$
 szMemChkTitle   equ     _szMemChkTitle  - $$
@@ -97,7 +116,7 @@ ARDStruct       equ     _ARDStruct   - $$
     dwType          equ _dwType     - $$
 MemChkBuf       equ     _MemChkBuf  - $$
 
-
+PageTableNumber equ     _PageTableNumber- $$
 
 
 DataLen         equ    ($ - LABEL_DATA)
@@ -133,9 +152,7 @@ LABEL_BEGIN:
         mov    sp, 0100h       
 
         mov [LABEL_GO_BACK_TO_REAL + 3], ax
-        mov [_wSPValueInRealMode], sp    ; 实模式下面所有label都能直接用
-                                         ; 因为实模式下面，不分段，label的地址就是offset
-                                         ; 保护模式下offset=(label-$$), 要通过selector:offset访问
+        mov [_wSPValueInRealMode], sp    ; 实模式下: label=从文件最开始算起的offset
 
         ; 通过15h中断，得到内存数
         mov    ebx, 0            ; ebx中存放后续值，第一次调用给0
@@ -265,8 +282,6 @@ LABEL_REAL_ENTRY:		; 从保护模式跳回到实模式就到了这里
 LABEL_SEG_CODE32:
 	    mov    ax, SelectorData
         mov    ds, ax          ; 数据段选择子->ds，保护模式的段地址都是Selector
-
-        mov    ax, SelectorData
         mov    es, ax          ; 数据段选择子->ds，保护模式的段地址都是Selector
 
         mov    ax, SelectorVideo
@@ -292,14 +307,14 @@ LABEL_SEG_CODE32:
         ; 显示内存信息
         call   DispMemSize
 
-        ; TODO: 启动分页机制
-        ;call   SetupPaging
+        ; 演示改变页目录的效果
+        call   PagingDemo
 
         ; 到此停止，跳到16位代码段，准备回到实模式
         jmp SelectorCode16:0
 
 ; ------------------------------------------------------------------------
-; TODO: SetupPaging: 启动分页机制
+; SetupPaging: 启动分页机制
 ;
 ;     为了简化处理，所有线性地址对应相等的物理地址，即F(0x12345678) = 0x12345678
 ;     假设线性地址0x12345678，经过分页管理机制转换以后，对应的物理地址正好也是0x12345678
@@ -307,18 +322,46 @@ LABEL_SEG_CODE32:
 
 SetupPaging:
         
+        ; 根据内存大小计算应该初始化多少PDE以及多少页表
+        xor    edx, edx
+
+        mov    eax, [dwMemSize]
+        mov    ebx, 400000h        ; 400000 = 4M = 4096 * 1024, 一个页表对应的内存大小
+
+        div    ebx
+
+        ; 打印商出来看看
+        ;push    eax
+        ;call    DispInt
+        ;pop     eax
+        ; 打印余数出来看看
+        ;push    edx
+        ;call    DispInt
+        ;pop     edx
+
+        mov    ecx, eax            ; 商在eax中，挪到ecx中
+
+        test   edx, edx
+        
+        jz     .no_remainder
+        inc    ecx                 ; 如果余数不为0，增加一个页表
+
+.no_remainder
+        mov    [PageTableNumber], ecx ; 将页表数目存入到PageTableNumber中
+
+        ; 为简化处理, 所有线性地址对应相等的物理地址. 并且不考虑内存空洞.
+
         ; 初始化PDE
-        mov    ax, SelectorPageDir    ; 该段首地址位PageDirBase
+        mov    ax, SelectorFlatRW    ; 该段首地址=0
         mov    es, ax
         
-        mov    ecx, 1024              ; 1024个表项
-        
-        xor    edi, edi
+        mov    edi, PageDirBase0     ; 偏移地址为PageDirBase0
+
         xor    eax, eax
 
         ; PageTblBase是TBL的入口地址，32位，但是页表项是4k对齐，所以低20位必然位0
         ; 因此低20位可以用来标识该页的一些基本属性，后面3个属性是存在、可读写、用户表。
-        mov    eax, PageTblBase | PG_P | PG_USU | PG_RWW
+        mov    eax, PageTblBase0 | PG_P | PG_USU | PG_RWW
 
 .1:
         ; STOSB/STOSW/STOSD 
@@ -332,18 +375,20 @@ SetupPaging:
         loop   .1
 
         ; 再初始化所有PTE (1k个页表，每个表里面有1k个PTE，每个PTE=4B，所以是4M)
-        mov    ax, SelectorPageTbl    ; 该段首地址位PageTblBase
-        mov    es, ax
+        mov    eax, [PageTableNumber]  ; 获取页表个数
+        mov    ebx, 1024
+        mul    ebx
 
-        mov    ecx, 1024 * 1024       ; 1k * 1k
+        mov    ecx, eax            ; PTE个数 = PDE个数 * 1024
 
-        xor    edi, edi
+        mov    edi, PageTblBase0   ; ; 偏移地址为PageTblBase0
+
         xor    eax, eax
 
         ; 这里是指定每一个页的首地址
         ; 第一个页的首地址是0x00000000，下面没有将0x00000000写上，看起来像是没有地址一样。
-        ; TODO: 修改地址，不使用0x0000000作为页的起始地址，挪动一两个字节看看
-        ; 测试结果，修改地址也起不到预想的作用，现在想不明白，等pmtest8弄完以后再回头看看
+        ; zf: 修改地址，不使用0x0000000作为页的起始地址，挪动一两个字节看看
+        ; 测试结果，修改地址也起不到预想的作用，因为4k对齐的问题，所以修改的地址必须大于4k
         mov    eax, PageTestBase | PG_P | PG_USU | PG_RWW
 
 .2:
@@ -353,7 +398,7 @@ SetupPaging:
 
         ; 准备开启分页
         ; 首先，让cr3指向PDE的首地址
-        mov    eax, PageDirBase
+        mov    eax, PageDirBase0
         mov    cr3, eax
 
         ; 设置cr0的PG位，开启分页
@@ -372,6 +417,235 @@ SetupPaging:
 
         ret
 ; SetupPaging 结束---------------------------------------------------------
+
+
+
+; ------------------------------------------------------------------------
+; PagingDemo: 测试分页机制
+;
+;     测试切换分页
+; ------------------------------------------------------------------------
+PagingDemo:
+    
+    mov    ax, cs     ; cs里面现在存的是SelectorCode32, 将SelectorCode32放入ds
+    mov    ds, ax     ; 因为要复制的函数PagingDemoProc、foo、bar都在这个32位代码段中
+
+    mov    ax, SelectorFlatRW
+    mov    es, ax     ; SelectorFlatRW是要复制的目标段的选择子，这个段是4G可读写的段
+
+    ; 复制函数foo到ProcFooAddr(0x00401000h)位置处
+    push   LenFoo
+    push   OffsetFoo
+    push   ProcFooAddr
+    call   MemCpy
+    add    esp, 12
+
+    ; 复制函数bar到ProcBarAddr(0x00501000h)位置处
+    push   LenBar
+    push   OffsetBar
+    push   ProcBarAddr
+    call   MemCpy
+    add    esp, 12
+
+    ; 复制函数PagingDemoProc到ProcPagingDemoAddr(0x00301000h)位置处
+    push   LenPagingDemoAll
+    push   OffsetPagingDemoProc
+    push   ProcPagingDemoAddr
+    call   MemCpy
+    add    esp, 12
+
+    mov    ax, SelectorData
+    mov    ds, ax
+    mov    es, ax
+
+    call   SetupPaging    ; 启动分页
+
+    ; 打印cs寄存器进行测试
+    ;xor     eax, eax
+    ;mov     ax, cs
+    ;push    eax
+    ;call    DispInt
+    ;pop     eax
+
+    ; zf: 测试一下直接call foo和bar函数的地址，不用调用PagingDemoProc函数的区别
+    ; 测试结果：可以直接call，但是需要将foo的返回改成retf
+    call   SelectorFlatC:ProcPagingDemoAddr    ; 这里SelectorFlatC这个段基址=0，所以起作用的就是偏移地址
+
+    call   PSwitch        ; 切换页目录，改变地址映射关系
+    
+    call   SelectorFlatC:ProcPagingDemoAddr
+
+    ret
+
+; PagingDemo 结束---------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; PSwitch: 切换页表
+; ------------------------------------------------------------------------
+PSwitch:
+    
+    ; 初始化PDE
+    mov    ax, SelectorFlatRW    ; 该段首地址=0
+    mov    es, ax
+    
+    mov    edi, PageDirBase1     ; 偏移地址为PageDirBase1
+
+    xor    eax, eax
+
+    ; PageTblBase是TBL的入口地址，32位，但是页表项是4k对齐，所以低20位必然位0
+    ; 因此低20位可以用来标识该页的一些基本属性，后面3个属性是存在、可读写、用户表。
+    mov    eax, PageTblBase1 | PG_P | PG_USU | PG_RWW
+
+    mov    ecx, [PageTableNumber]
+
+.1:
+    ; STOSB/STOSW/STOSD 
+    ; AL、AX、EAX中的Byte、word或dword存储到"ES:EDI"或"ES:DI"的内存地址中去
+    ; 以STOSD为例子：
+    ; DF=0:   eax->es:edi     edi+4  
+    ; DF=1:   eax->es:edi     edi-4  
+    stosd
+
+    add    eax, 4096      ; 每个PTE是4k，所以地址+4k
+    loop   .1
+
+    ; 再初始化所有PTE (1k个页表，每个表里面有1k个PTE，每个PTE=4B，所以是4M)
+    mov    eax, [PageTableNumber]  ; 获取页表个数
+    mov    ebx, 1024
+    mul    ebx
+
+    mov    ecx, eax            ; PTE个数 = PDE个数 * 1024
+
+    mov    edi, PageTblBase1   ; 偏移地址为PageTblBase1
+
+    xor    eax, eax
+
+    ; 这里是指定每一个页的首地址
+    mov    eax, PageTestBase | PG_P | PG_USU | PG_RWW
+
+.2:
+    stosd
+    add    eax, 4096      ; 每个页是4k，所以地址+4k
+    loop   .2
+
+    ; 在此假设内存是大于 8M 的
+
+    ; 0x00401000,转化为2进制为：0000，0000，0100，0000，0001，0000，0000，0000
+    mov    eax, LinearDemoAddr
+    shr    eax, 22             ; eax变为0x00000001
+    mov    ebx, 4096
+    mul    ebx                 ; eax变为0x00001000
+
+    mov    ecx, eax            ; ecx=0x00001000
+
+    ; 0x00401000,转化为2进制为：0000，0000，0100，0000，0001，0000，0000，0000
+    mov    eax, LinearDemoAddr            
+    shr    eax, 12                        ; eax=0x00000401
+    and    eax, 03FFh                     ; 1111111111b (10 bits)  eax=0x00000001
+    mov    ebx, 4                         ; ebx=0x00000004
+    mul    ebx                            ; eax=0x00000004
+
+    add    eax, ecx                       ; eax=0x00001004
+
+    add    eax, PageTblBase1              ; add eax,0x00211000,eax变为0x00212004
+
+    ; [es:eax]指向SelectorFlatRW的0x00212004,这句肯定把原来函数地址变了。
+    mov    dword [es:eax], ProcBarAddr | PG_P | PG_USU | PG_RWW
+
+
+    ; 首先，让cr3指向PDE1的首地址
+    mov    eax, PageDirBase1
+    mov    cr3, eax
+
+    ; zf: 这个jmp的作用是什么？去掉是否可以?
+    ; 测试结果：去掉没有问题。。。。先忽略吧。。。。
+    ;jmp    short .3
+
+.3
+    ; zf: 这个nop的作用？？？
+    ; 测试结果：去掉没有问题。。。。先忽略吧。。。。
+    ;nop
+
+    ret
+
+; PSwitch 结束---------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; PagingDemoProc: 调用LinearDemoAddr地址的函数，LinearAddrDemo由页表查询
+; ------------------------------------------------------------------------
+PagingDemoProc:
+OffsetPagingDemoProc    equ    PagingDemoProc - $$
+    
+    ; zf：测试一下直接call，不使用eax会有什么区别???
+    ; 测试结果：call SelectorFlatC:LinearDemoAddr可以执行，call LinearDemoAddr不行
+    ; 注意：这里LinearDemoAddr是一个绝对地址，而上面的比如call PSwitch中的PSwitch是label
+    ; call  Label   -----  IP=IP+16位偏移量(16位位移由编译程序在编译时算出)
+    ; call  imm     -----  IP=IP+imm(是一个相对当前位置的偏移量，和label的区别就是指定的偏移量)
+    ; call  reg     -----  IP=reg
+
+    ; call reg 是直接跳转到寄存器值所指的偏移位置（这里的偏移是相对于段，也就是说，对段内而言，这种方式直接跳转到一个绝对地址，而不是与当前位置的偏移） 
+    mov    eax, LinearDemoAddr
+    call   eax                    
+
+    ; call imm 是一个相对当前位置的偏移量
+    ;call    LinearDemoAddr
+
+    ; 这是可以成功的，但是必须retf返回
+    ; zf: 测试下面这种调用方式是否触发分页
+    ; 测试结果：可以触发分页
+    ;call    SelectorFlatC:LinearDemoAddr
+
+    retf              ; retf 返回是因为调用的时候使用的是"call selector:offset"的方式
+
+LenPagingDemoAll    equ    $ - PagingDemoProc
+; PSwitch 结束---------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; foo: 打印Foo
+; ------------------------------------------------------------------------
+foo:
+OffsetFoo    equ    foo - $$
+    
+    mov    ah, 0Ch        ; 0000: 黑底    1100: 红字
+
+    mov    al, 'F'
+    mov    [gs:((80*17 + 0) * 2)], ax    ; 屏幕第 17 行, 第 0 列。
+
+    mov    al, 'o'
+    mov    [gs:((80*17 + 1) * 2)], ax    ; 屏幕第 17 行, 第 1 列。
+    mov    [gs:((80*17 + 2) * 2)], ax    ; 屏幕第 17 行, 第 2 列。
+
+    ret
+
+LenFoo    equ    $ - foo
+; foo 结束---------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+; bar: 打印Bar
+; ------------------------------------------------------------------------
+bar:
+OffsetBar    equ    bar - $$
+    
+    mov    ah, 0Ch        ; 0000: 黑底    1100: 红字
+
+    mov    al, 'B'
+    mov    [gs:((80*18 + 0) * 2)], ax    ; 屏幕第 18 行, 第 0 列。
+
+    mov    al, 'a'
+    mov    [gs:((80*18 + 1) * 2)], ax    ; 屏幕第 18 行, 第 1 列。
+
+    mov    al, 'r'
+    mov    [gs:((80*18 + 2) * 2)], ax    ; 屏幕第 18 行, 第 2 列。
+    
+    ret
+
+LenBar    equ    $ - bar
+; bar 结束---------------------------------------------------------
+
 
 
 ; ------------------------------------------------------------------------
---
+++ chapter3/pmtest8/test_com.sh
@@ -0,0 +1,26 @@
+# sh脚本，用来编译$FileName.asm并且生成$FileName.com文件，并且导入dos软盘中，启动dos系统
+
+# 设置要使用nasm编译的文件名，不能带后缀名
+FileName=pmtest8
+
+echo 用来编译$FileName.asm并且生成$FileName.com文件，并且导入dos软盘中，启动dos系统的 by zf
+
+echo ====== gen .com file
+nasm "$FileName".asm -o "$FileName".com
+
+echo +++++++++++ mount b.img
+hdiutil mount ~/Bochs/fdos-10meg/b.img
+
+echo ------ copy .com file
+cp ./"$FileName".com /Volumes/NO\ NAME/
+
+echo +++++++++++ unmount b.img
+
+hdiutil unmount /Volumes/NO\ NAME/
+
+echo ^^^^^^ run Bochs
+cd ~/Bochs/fdos-10meg/
+
+bochs -q -f bochsrc.txt
+
+echo @@@@@@ finish
